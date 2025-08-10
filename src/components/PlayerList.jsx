04ac67a377bf0bfeeb1542faca855ffba3477eea
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-08-10 16:12:11 -0600

    finalised each component to render players and matches

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -2,12 +2,21 @@ import { Player } from "./Player";
  
 export const PlayerList = (props) => {
 
-  const onePlayer = props.playerData[0];
+  const players = props.playerData.map((player) => {
+    return (
+      <Player 
+        key={player.gamerTag}
+        firstName={player.firstName} 
+        lastName={player.lastName} 
+        gamerTag={player.gamerTag} 
+        wins={player.wins}/>
+    )
+  })
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player firstName={onePlayer.firstName} lastName={onePlayer.lastName} gamerTag={onePlayer.gamerTag} wins={onePlayer.wins}/>
+      {players}
     </section>
   )
 }
